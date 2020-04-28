6f7e58c55bbe84dedee19376e507a4be1dfb0cc2
Diego <diego@breda.build>
2020-04-28 19:53:58 +0200

    bitcoin address done

--- src/modules/chain/transactions/transaction.service.ts
+++ src/modules/chain/transactions/transaction.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import * as winston from 'winston';
 import * as crypto from "crypto";
-import * as Base58 from "base-58";
+import * as base58 from "base-58";
 import * as secp256k1 from 'secp256k1'
-import { Signer } from "crypto";
+import { ECDH  } from 'crypto';
 
 // interface Input {
 //   previous_output: {
@@ -24,50 +24,58 @@ import { Signer } from "crypto";
 
 // https://en.bitcoin.it/wiki/Wallet_import_format
 export async function getAddress() {
-// generate privKey
-  let privKey
+  console.log('Generate private key');
+  let privKey: any;
   do {
-    privKey = crypto.randomBytes(32)
-    privKey = 'f19c523315891e6e15ae0608a35eec2e00ebd6d1984cf167f46336dabd9b2de4';
+    privKey = 'F19C523315891E6E15AE0608A35EEC2E00EBD6D1984CF167F46336DABD9B2DE4';
     privKey = Buffer.from(privKey, 'hex');
     // privKey = Uint8Array.from(Buffer.from('d2e434b74d08afebf787d63f4cde9b28f9ccb7fa9ddbe2eaffb48aaf9a3017ca'));
   } while (!secp256k1.privateKeyVerify(privKey));
-  console.log('Private Key: ' + privKey.toString('hex'));
+  const privKeyHex = privKey.toString('hex');
+  console.log(privKeyHex);
 
-  // get the public key in a compressed format
-  const pubKey = secp256k1.publicKeyCreate(privKey);
-  console.log('Public Key: ' + (new Buffer(pubKey)).toString('hex'));
+  // {
+  {
+    console.log('Generate Public Key');
+    const ecdh: ECDH = crypto.createECDH('secp256k1');
+    ecdh.setPrivateKey(privKey);
+    const publicKey = ecdh.getPublicKey();
+    console.log(ecdh.getPublicKey().toString('hex'));
 
-  const privKeyWif = Base58.encode('0x80' + privKey);
+    console.log('Private Key to Address');
+    let sha256 = crypto.createHash('sha256');
+    let hash: any = sha256.update(Buffer.from(ecdh.getPublicKey().toString('hex'), 'hex')).digest();
+    const ripemd160 = crypto.createHash('ripemd160');
+    let hashToRipmd160 = ripemd160.update(Buffer.from(hash.toString('hex'), 'hex')).digest();
+    hashToRipmd160 = Buffer.from("00" + hashToRipmd160.toString('hex'), 'hex');
+    sha256 = crypto.createHash('sha256');
+    hash = sha256.update(Buffer.from(hashToRipmd160.toString('hex'), 'hex')).digest();
+    sha256 = crypto.createHash('sha256');
+    hash = sha256.update(Buffer.from(hash, 'hex')).digest('hex');
+    const checksum = hash.substring(0, 8);
+    const hashcs: any = hashToRipmd160.toString('hex') + checksum;
+    const address = base58.encode(Buffer.from(hashcs, 'hex'));
+    console.log(address.toString('hex'));
+  }
+  {
+    console.log('Private Key To WIF');
+    const step1 = Buffer.from("80" + privKeyHex, 'hex');
+    // step 2 - create SHA256 hash of step 1
+    let sha256 = crypto.createHash('sha256');
+    const step2 = sha256.update(step1).digest('hex');
+    // step 3 - create SHA256 hash of step 2
+    sha256 = crypto.createHash('sha256');
+    const step3 = sha256.update(Buffer.from(step2, 'hex')).digest('hex');
+    // step 4 - find the 1st byte of step 3 - save as "checksum"
+    const checksum = step3.substring(0, 8);
+    // step 5 - add step 1 + checksum
+    const step4 = step1.toString('hex') + checksum;
+    // return base 58 encoding of step 5
+    const address = base58.encode(Buffer.from(step4, 'hex'));
+    console.log(address);
+  }
 
-  const ripemd160WithRSA = crypto.createHash('ripemd160WithRSA');
-  ripemd160WithRSA.update(pubKey);
-  const address = Base58.encode('0x80' + ripemd160WithRSA.digest('hex'));
-  console.log('Address: ' + address);
 
-
-
-  // const wallet: any = Base58.encode('0x80' + privateKey);
-  // console.log(wallet);
-  // // console.log(crypto.getHashes())
-  // const sign = crypto.createSign('sha256');
-  // sign.update('some data to sign');
-  // const privatekey = crypto.crea(privateKeyEncode)
-  // try {
-  //   const signature = sign.sign(privateKey, 'hex');
-  //   console.log(signature);
-  //
-  // } catch (e) {
-  //   console.log('ERROL');
-  //   console.log(e);
-  // }
-  // console.log(privateKey);
-  // privateKey = '80' + privateKey;
-  // console.log(privateKey.toString('hex'));
-  // console.log(privateKey.toString('base64'));
-
-  // const hash: any = crypto.createHash('sha256').update(bytes).digest('hex');
-  // console.log(hash);
 
 }
 
